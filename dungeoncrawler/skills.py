2fb81a2ac786ac96ad9f362400859e5eaf623f1a
Joe H. Rahme <jhakimra@redhat.com>
2020-09-19 01:16:33 +0200

    Fixes Ability names
    Fixes #8

--- dungeoncrawler/skills.py
+++ dungeoncrawler/skills.py
@@ -21,13 +21,14 @@ from dungeoncrawler import utils
 
 class Ability(abc.ABC):
     mp_cost = 100
+    name = "Ability"
 
     def __init__(self, world, caster):
         self.world = world
         self.caster = caster
 
     def __repr__(self):
-        return str(type(self).__name__)
+        return self.name
 
     def predicate(self):
         return self.caster.mp == self.caster.maxMP
@@ -74,6 +75,7 @@ class Ability(abc.ABC):
 
 class ATTACK(Ability):
     mp_cost = 0
+    name = "ATTACK"
 
     def predicate(self):
         return True
@@ -135,6 +137,7 @@ class ATTACK(Ability):
 
 class WellIntentionedWish(Ability):
     mp_cost = 70
+    name = "Wishful Intention"
 
     def targets(self, combo):
         _targets = []
@@ -163,8 +166,20 @@ class WellIntentionedWish(Ability):
             target.name, utils.bold(str(amount)), old_value, new_value))
 
 
+class WellIntentionedWish2(WellIntentionedWish):
+    mp_cost = 100
+
+    def targets(self, combo):
+        _target = []
+        for unit in combo:
+            if unit in self.world.yourteam and 0 < unit.hp_ratio:
+                _target.append(unit)
+        return _target
+
+
 class ThousandFists(Ability):
     mp_cost = 120
+    name = "A Thousand Fists"
 
     def targets(self, combo):
         _targets = (h for h in self.world.enemyteam if h.hp)
@@ -191,6 +206,7 @@ class ThousandFists(Ability):
 
 class SilentPrayer(Ability):
     mp_cost = 70
+    name = "Silent Prayer"
 
     def targets(self, combo):
         _targets = []
@@ -215,6 +231,7 @@ class SilentPrayer(Ability):
 
 class NovaBlast(Ability):
     mp_cost = 120
+    name = "Nova Blast"
 
     def opening_words(self):
         print(r"""
@@ -248,6 +265,7 @@ class NovaBlast(Ability):
 
 class Focus(Ability):
     mp_cost = 60
+    name = "Sharp Focus"
 
     def targets(self, combo):
         return [self.caster]
@@ -265,6 +283,7 @@ class Focus(Ability):
 
 class BurstingQi(Ability):
     mp_cost = 80
+    name = "Burning Qi"
 
     def targets(self, combo):
         return [self.caster]
@@ -314,6 +333,7 @@ class CuriousBox(Ability):
 
 class BootyTrap(Ability):
     mp_cost = 100
+    name = "Booty Trap"
 
     def targets(self, combo):
         eligible = [m for m in self.world.enemyteam if m.hp]
@@ -343,6 +363,7 @@ class BootyTrap(Ability):
 
 class ChivalrousProtection(Ability):
     mp_cost = 100
+    name = "Chivalrous Protection"
 
     def targets(self, combo):
         return self.world.yourteam
@@ -361,6 +382,7 @@ class ChivalrousProtection(Ability):
 
 class RighteousInspiration(Ability):
     mp_cost = 100
+    name = "Righteous Inspiration"
 
     def targets(self, combo):
         return [h for h in self.world.yourteam if (h.mp * h.hp)]
@@ -377,6 +399,8 @@ class RighteousInspiration(Ability):
 
 
 class BubblyPickMeUp(Ability):
+    name = "Bubbly Pick-me-up"
+
     def predicate(self):
         return any((mob.hp_ratio < 60 for mob in self.world.enemyteam))
 
@@ -398,6 +422,8 @@ class BubblyPickMeUp(Ability):
 
 
 class TemporaryInsanity(Ability):
+    name = "Temporary Insanity"
+
     def predicate(self):
         return True
 
@@ -415,6 +441,7 @@ class TemporaryInsanity(Ability):
 
 
 class AngryOwner(Ability):
+    name = "Angry Owner"
 
     def predicate(self):
         if len(self.world.enemyteam) == 1:
@@ -446,3 +473,123 @@ class AngryOwner(Ability):
                 utils.color_yellow(str(self)),
                 target.name, utils.bold(str(amount)),
                 old_value, new_value))
+
+
+class LieDownAndBleed(Ability):
+    name = "Lie Down and Bleed"
+
+    def predicate(self):
+        return random.random() <= 0.1
+
+    def targets(self, combo):
+        _t = []
+        for h in combo:
+            if h in self.world.yourteam and h.hp:
+                _t.append(h)
+        if not _t:
+            for h in self.world.yourteam:
+                if h.hp:
+                    _t.append(h)
+        return [max(_t, key=lambda x: x.hp_ratio)]
+
+    def single_effect(self, target):
+        dmg = self.physical_damage(self.caster, target, 3.5)
+        _oldt = target.hp
+        target.hp -= dmg
+        return (dmg, _oldt, target.hp)
+
+    def log_skill(self, amount, target, old_value, new_value):
+        utils.slow_type(
+            "%s: [%s] on %s dealing %s dmg. (%d -> %d)\n" % (
+                utils.bold(utils.color_red(self.caster.name)),
+                utils.color_yellow(str(self)),
+                target.name, utils.bold(str(amount)),
+                old_value, new_value))
+
+
+class ClashingAndSlashing(Ability):
+    name = "Clashing and Slashing"
+
+    def predicate(self):
+        return True
+
+    def targets(self, combo):
+        _t = []
+        for h in combo:
+            if h in self.world.yourteam and h.hp:
+                _t.append(h)
+        return _t
+
+    def single_effect(self, target):
+        return ATTACK(self.world, self.caster).single_effect(target)
+
+    def log_skill(self, amount, target, old_value, new_value):
+        utils.slow_type(
+            "%s: [%s] on %s dealing %s dmg. (%d -> %d)\n" % (
+                utils.bold(utils.color_red(self.caster.name)),
+                utils.color_yellow(str(self)),
+                target.name, utils.bold(str(amount)),
+                old_value, new_value))
+
+
+class FullOfZeal(Ability):
+    name = "Full of Zeal"
+
+    def predicate(self):
+        return random.random() <= 0.3
+
+    def targets(self, combo):
+        _t = []
+        for h in combo:
+            if h in self.world.yourteam and h.hp:
+                _t.append(h)
+        if not _t:
+            _t = self.world.yourteam
+        return [random.choice(_t)]
+
+    def single_effect(self, target):
+        dmg = self.physical_damage(self.caster, target, 0.7)
+        _oldt = target.hp
+        target.hp -= dmg
+        self.caster.SPD *= 1.3
+        return (dmg, _oldt, target.hp)
+
+    def log_skill(self, amount, target, old_value, new_value):
+        utils.slow_type(
+            "%s: [%s] on %s dealing %s dmg. (%d -> %d)\n" % (
+                utils.bold(utils.color_red(self.caster.name)),
+                utils.color_yellow(str(self)),
+                target.name, utils.bold(str(amount)),
+                old_value, new_value))
+        utils.slow_type(
+            "%s: [%s] increases self SPD.\n" % (
+                utils.bold(utils.color_red(self.caster.name)),
+                utils.color_yellow(str(self))))
+
+
+class BoastfulNoMore(Ability):
+    name = "Boastful No More"
+
+    def predicate(self):
+        return any(40 < h.mp_ratio < 70 for h in self.world.yourteam)
+
+    def targets(self, combo):
+        _t = list(reversed(sorted(
+            (h for h in self.world.yourteam if h.mp_ratio != 100),
+            key=lambda x: x.mp_ratio)))
+        if _t:
+            return [_t[0]]
+        else:
+            return random.choice(
+                [h for h in combo if h in self.world.yourteam and h.hp])
+
+    def single_effect(self, target):
+        target.mp = 0
+        return ('', '', '')
+
+    def log_skill(self, amount, target, old_value, new_value):
+        utils.slow_type(
+            "%s: [%s] absorbed mp of %s\n" % (
+                utils.bold(utils.color_red(self.caster.name)),
+                utils.color_yellow(str(self)), target.name
+            ))
